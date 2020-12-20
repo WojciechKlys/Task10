77a676063b163296e6960983cc7ce304f90c77e8
Wojtek Klys <wojciechklys@gmail.com>
2020-12-20 22:55:56 +0100

    final version

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var first = 10
-var second = 5
-var third = 2
+var first = 10;
+var second = 5;
+var third = 2;
 
 function add(a, b) {
     return a + b;
 }
 
+function divide(a, b) {
+    return a / b;
+}
+
 function doCalculation() {
-    let result = add(first, second) / third;
-    console.log(result)
+    let result = divide(add(first, second), third);
+    console.log(result);
 }
 
 doCalculation();
