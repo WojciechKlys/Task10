0467cb94ae0354318fd7903c732d05dd046001db
Wojtek Klys <wojciechklys@gmail.com>
2020-12-20 19:33:06 +0100

    change1

---
+++ script.js
@@ -0,0 +1,15 @@
+var first = 10
+var second = 5
+var third = 2
+
+function add(a, b) {
+    return a + b;
+}
+
+function doCalculation() {
+    let result = add(first, second) / third;
+    console.log(result)
+}
+
+doCalculation();
+
